4ccafc51422206f2679bfc0bc273113b6c0dbba1
Leonid <enykoruna1@gmail.com>
2023-05-29 14:15:57 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Azure;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http.Json;
+using System.Net.Mail;
+using System.Numerics;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json;
+using System.Xml;
+using System.Xml.Serialization;
+using Telegram.Bot.Types;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 10;
+        Console.WriteLine(x);
+    }   
+}
